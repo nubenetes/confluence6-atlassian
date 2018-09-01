b2e7a84037e03b006b9d7eb5c6b8975f43f6df59
Iñaki Fernández <inafevwork@gmail.com>
2018-09-01 18:35:40 +0200

    umask in exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -54,6 +54,6 @@ if [ "${UID}" -eq 0 ]; then
 else
     echo "User is not root"
     #chmod g-s "${CONFLUENCE_HOME}" -> chmod: operation not permitted
-    exec "$CONFLUENCE_INSTALL_DIR/bin/start-confluence.sh" "$@"
+    exec "umask 0002" "$CONFLUENCE_INSTALL_DIR/bin/start-confluence.sh" "$@"
 fi
 
